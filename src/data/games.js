ee1c943ddf503c4cf1f22f9a7624ed2ccc2843ff
Daniel Whiffing <danielwhiffing@gmail.com>
2020-04-26 00:57:02 -0400

    Adds games

--- src/data/games.js
+++ src/data/games.js
@@ -1,6 +1,18 @@
 import VagrantImage from './../assets/images/vagrant.png'
 import SolitaireImage from './../assets/images/solitaire.png'
 import TawatomoImage from './../assets/images/tawatomo.png'
+import PokerImage from './../assets/images/poker.png'
+import HexacrossImage from './../assets/images/hexacross.png'
+import StellarPedulumImage from './../assets/images/stellar-pendulum.jpg'
+import SnakeBakeImage from './../assets/images/snake-n-bake.jpg'
+import StampedeImage from './../assets/images/stampede.jpg'
+import SudokuImage from './../assets/images/sudoku.png'
+import FuseImage from './../assets/images/fuse.jpg'
+import IdleImage from './../assets/images/idle.png'
+import BodyBrigadeImage from './../assets/images/body-brigade.png'
+import SiegeImage from './../assets/images/siege.png'
+import VengeanceImage from './../assets/images/vengeance.png'
+import IncrementalImage from './../assets/images/incremental.png'
 
 const data = [
   {
@@ -20,6 +32,78 @@ const data = [
     website: 'https://dwhiffing.github.io/tawatomo',
     description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At augue eget arcu dictum varius duis.`,
     image: TawatomoImage,
+  },
+  {
+    title: 'Poker',
+    website: 'http://daniel-poker.herokuapp.com/',
+    description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At augue eget arcu dictum varius duis.`,
+    image: PokerImage,
+  },
+  {
+    title: 'Hexacross',
+    website: 'https://dwhiffing.github.io/hexacross',
+    description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At augue eget arcu dictum varius duis.`,
+    image: HexacrossImage,
+  },
+  {
+    title: 'Snake & Bake',
+    website: 'http://dwhiffing.github.io/snakebake/',
+    description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At augue eget arcu dictum varius duis.`,
+    image: SnakeBakeImage,
+  },
+  {
+    title: 'Stampede',
+    website: 'https://dwhiffing.github.io/stampede/',
+    description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At augue eget arcu dictum varius duis.`,
+    image: StampedeImage,
+  },
+  {
+    title: 'Stellar Pendulum',
+    website: 'http://dwhiffing.github.io/stellar-pendulum/',
+    description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At augue eget arcu dictum varius duis.`,
+    image: StellarPedulumImage,
+  },
+  {
+    title: 'Sudoku',
+    website: 'http://dwhiffing.github.io/sudoku/',
+    description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At augue eget arcu dictum varius duis.`,
+    image: SudokuImage,
+  },
+  {
+    title: 'Idle Game',
+    website: 'http://dwhiffing.github.io/idle-game/',
+    description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At augue eget arcu dictum varius duis.`,
+    image: IdleImage,
+  },
+  {
+    title: 'Body Brigade',
+    website: 'http://dwhiffing.github.io/games-on-demand/',
+    description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At augue eget arcu dictum varius duis.`,
+    image: BodyBrigadeImage,
+  },
+  {
+    title: 'Siege',
+    website: 'https://dwhiffing.github.io/ludumdare-2016/',
+    description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At augue eget arcu dictum varius duis.`,
+    image: SiegeImage,
+  },
+  {
+    title: 'Vengeance With No Goal',
+    website: 'http://dwhiffing.github.io/vengeance-with-no-goal/',
+    description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At augue eget arcu dictum varius duis.`,
+    image: VengeanceImage,
+  },
+  {
+    title: 'Incremental',
+    website: 'http://dwhiffing.github.io/incremental/',
+    description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At augue eget arcu dictum varius duis.`,
+    image: IncrementalImage,
+  },
+  {
+    title: 'Fuse',
+    website: 'http://dwhiffing.github.io/fuse/',
+    description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At augue eget arcu dictum varius duis.`,
+    image: FuseImage,
   },
 ]
 
